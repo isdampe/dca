0ce6d919bb0a9549654c1a50a7ec1d2bc4a42fc6
Richard Denton <isdampe@gmail.com>
2018-05-20 11:59:28 +1000

    ENHANCE: Add basic EFP photon controls

--- photon/dca-slave/src/efp.h
+++ photon/dca-slave/src/efp.h
@@ -3,9 +3,9 @@
 #include <stdint.h>
 
 //EFP - Efficient ........ protocol.
-#define EFP_CMD_REGISTER_BYTE 0x1
-#define EFP_CMD_REGISTER_SLAVE_ACK_BYTE 0x2
-#define EFP_CMD_REGISTER_DATA_BYTE 0x3
+#define EFP_CMD_REGISTER_BYTE 0x0
+#define EFP_CMD_REGISTER_SLAVE_ACK_BYTE 0x1
+#define EFP_CMD_REGISTER_DATA_BYTE 0x2
 
 #define EFP_SLAVE_ADDR 0x10
 #define EFP_SLAVE_REGISTERS 0x2
@@ -34,8 +34,13 @@ typedef struct
 	uint8_t current_job_start_idx;
 	uint8_t current_job_progress = 0x0;
 	uint8_t current_job_results[EFP_JOB_FACTOR];
+	uint16_t reg_val;
+	uint8_t registers[4];
 } efp_slave;
 
 void efp_slave_init(efp_slave *slave);
+void efp_slave_parse_registers(const uint32_t reg_val, efp_slave *slave, uint8_t reg_addr);
+void efp_dump_registers(const efp_slave *slave);
+uint32_t efp_pack_registers(const efp_slave *slave);
 
 #endif
--- photon/dca-slave/src/efp.ino
+++ photon/dca-slave/src/efp.ino
@@ -9,4 +9,32 @@ void efp_slave_init(efp_slave *slave)
 
 	for (uint8_t i=0; i<EFP_JOB_FACTOR; ++i)
 		slave->current_job_results[i] = 0x0;
+
+	for (uint8_t i=0; i<4; ++i)
+		slave->registers[i] = 0x0;
+}
+
+void efp_slave_parse_registers(const uint32_t reg_val, efp_slave *slave, uint8_t reg_addr)
+{
+	for (uint8_t i=0x0; i<0x4; ++i)
+		slave->registers[i] = (reg_val >> (i * 8) & 0xff);
+	
+	efp_dump_registers(slave);
+}
+
+void efp_dump_registers(const efp_slave *slave)
+{
+	for (uint8_t i=0x0; i<0x4; ++i)
+		Serial.printlnf("Reg %i: 0x%02x", i, slave->registers[i]);
+	Serial.printlnf("");
+}
+
+uint32_t efp_pack_registers(const efp_slave *slave)
+{
+	uint32_t result = 0x0;
+	
+	for (uint8_t i=0x0; i<0x4; ++i)
+		result = (result << 8) | slave->registers[i];
+
+	return result;
 }
